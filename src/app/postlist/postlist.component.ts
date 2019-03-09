01e3bd6947e1efc0314fcb09052e5ddb835c848f
AlpineWebDev <alpinewebdev@gmail.com>
2019-03-09 19:22:16 +0100

    - fixed security vulnerabilities - fixed minor css problems

--- src/app/postlist/postlist.component.ts
+++ src/app/postlist/postlist.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Blogpost } from'../blogpost';
+import { Blogpost } from '../blogpost';
 import { PostService } from '../post.service';
 import { HttpClient } from '@angular/common/http';
 
@@ -24,20 +24,20 @@ export class PostlistComponent implements OnInit {
   }
 
   /**
-   * Is called when a blogpost title in the postlist is selected. Each blogpost title listens for the condition, 
+   * Is called when a blogpost title in the postlist is selected. Each blogpost title listens for the condition,
    * that it is the selected blogpost. Upon selection it a css class is added to this title,
    * which changes its background color to light grey.
-   * @param blogpost 
+   * @param blogpost
    */
   onSelect (blogpost: Blogpost): void {
-    this.selectedBlogpost = blogpost; 
+    this.selectedBlogpost = blogpost;
   }
 
   /**
    * Is called when one of the hearts besides the blogpost titles in the postlist is called. It sets the checked value
    * of the corresponding blogpost to one. The html heart label listens for this condition, and adds a css class to its
    * styles if the condition is met. The css class turns the color of the heart red.
-   * @param blogpost 
+   * @param blogpost
    */
   onSelectHeart(blogpost: Blogpost): void {
     if(blogpost.checked === 1){
@@ -49,7 +49,7 @@ export class PostlistComponent implements OnInit {
 
   /**
    * Calls the getBlogposts() function in the postService and subscribes to its returning observable.
-   * Asynchronous operation. Upon arrival of the data, another function (createPostArray()) is called, 
+   * Asynchronous operation. Upon arrival of the data, another function (createPostArray()) is called,
    * which assigns the data and adds the checked variable to the objects.
    */
   getBlogposts(): void {
@@ -60,7 +60,7 @@ export class PostlistComponent implements OnInit {
   /**
    * Is called upon arrival of the blogposts. Assigns the blogpost data and sets an additional variable checked to each
    * object in the array.
-   * @param posts 
+   * @param posts
    */
   createPostArray(posts): void {
     this.blogposts = posts;
